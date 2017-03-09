e6e19aaeeb115f5bffd8dad1a72ec16e16b4342d
Ilya Goryachev <ilya.gory@yandex.ru>
2017-03-09 16:30:38 +0300

    dist

--- app/routes.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {Route} from 'react-router';
-import InnerLayout from './containers/InnerLayout';
-
-export default [
-    <Route path="/" component={InnerLayout} />
-];
--- app/util/clone.js
+++
@@ -1,3 +0,0 @@
-export default function (x) {
-    return JSON.parse(JSON.stringify(x));
-}
